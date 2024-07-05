2a7801302dfdce9fc9e493d488baa01143d9b89c
adlizone <aadilazizlone647@gmail.com>
2024-07-05 05:28:17 -0700

    navbar

---
+++ home/static/js/navbar.js
@@ -0,0 +1,9 @@
+// navbar.js
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('navbar-toggle');
+    const menu = document.getElementById('navbar-menu');
+
+    toggleButton.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
