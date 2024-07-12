fdfb7372e567f0b55f113dcd3d7e3c08cb3714b9
adlizone <aadilazizlone647@gmail.com>
2024-07-12 06:48:12 -0700

    ...

--- kurooms/settings.py
+++ kurooms/settings.py
@@ -139,3 +139,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['dafault'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
