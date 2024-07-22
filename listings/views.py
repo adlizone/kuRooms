ee23de57c84d43b4d16aed22083985db6a92a78c
adlizone <aadilazizlone647@gmail.com>
2024-07-21 19:12:59 -0700

    Random Feed

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
+from django.views import View
 from django.http import HttpResponse
 from .models import Property
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 from .forms import PropertyForm
+import random
 
-class MainView(OwnerListView):
-    model = Property
-    fields = ["title","picture", "address", "contact_1", "contact_2","status", "rent_per_month"]
+class MainView(View):
+    
+    template_name = "listings/property_list.html"
+
+    def get(self, request):
+        properties = Property.objects.all()
+        properties = list(properties)
+        random.shuffle(properties)
+        ctx = {"property_list" : properties}    
+        return render(request, self.template_name, ctx)
 
 class PropertyCreate(OwnerCreateView):
     model = Property
